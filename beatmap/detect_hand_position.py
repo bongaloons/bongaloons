ef0ca8707e1eeb8430b8cb5aa41e43d11449d07e
Gabriel Tsai <gabrielhtsai@gmail.com>
2025-02-16 14:43:03 -0800

    fixed live detection

--- beatmap/detect_hand_position.py
+++ beatmap/detect_hand_position.py
@@ -5,7 +5,7 @@ import numpy as np
 import time
 from config import *
 from fastapi import FastAPI, WebSocket
-import uvicorn
+import uvicorn 
 
 app = FastAPI()
 
@@ -19,7 +19,7 @@ mp_drawing = mp.solutions.drawing_utils
 hands = mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5)
 interpreter, input_details, output_details = None, None, None
 
-def detect_hand_position_draw(frame, hands, mp_hands, mp_drawing):
+def detect_hand_position_draw(frame, interpreter, hands, mp_hands, mp_drawing, input_details, output_details, use_double=True):
     #Unmirror the frame
     frame = cv2.flip(frame, 1)
     # Convert BGR to RGB (MediaPipe requirement)
@@ -34,25 +34,13 @@ def detect_hand_position_draw(frame, hands, mp_hands, mp_drawing):
             x_vals = [lm.x for lm in hand_landmarks.landmark]
             y_vals = [lm.y for lm in hand_landmarks.landmark]
 
-            # Get average hand position
-            hand_x = np.mean(x_vals)
-            hand_y = np.mean(y_vals)
-
-            # Determine region
-            position = "Middle"
-            if hand_y < TOP_THRESHOLD:
-                position = "Top"
-            elif hand_y > BOTTOM_THRESHOLD:
-                position = "Bottom"
-
+            positions = detect_hand_position(frame, interpreter, hands, mp_hands, input_details, output_details, use_double=True)
             # Left hand position, then right hand position
-            positions.append(position)
 
+        if use_double:
             # Get hand label (Left or Right)
-            hand_label = handedness.classification[0].label  # "Left" or "Right"
-
-            text = f"{hand_label} Hand: {position}"
-            cv2.putText(frame, text, (50, 50 + idx * 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+            text = f"{DOUBLE_ID_TO_GESTURE[positions[0]]}"
+            cv2.putText(frame, text, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
 
     return frame, positions
 
@@ -65,7 +53,7 @@ def setup_model(tflite_save_path):
 
     return interpreter, input_details, output_details
 
-def detect_hand_position(frame, interpreter, hands, mp_hands, input_details, output_details, use_double=False):
+def detect_hand_position(frame, interpreter, hands, mp_hands, input_details, output_details, use_double=True):
     """
     Detect hand positions in a frame. For each hand, determine if it is in the top, middle, or bottom region of the screen.
     Call this function for base bongo hits. 
@@ -75,8 +63,8 @@ def detect_hand_position(frame, interpreter, hands, mp_hands, input_details, out
     # Convert BGR to RGB (MediaPipe requirement)
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     results = hands.process(frame_rgb)
-    positions = [None, None]
-
+    positions = [len(DOUBLE_LABEL_MAP)-1, len(DOUBLE_LABEL_MAP)-1]
+    print(f"Input details:{input_details}")
     # if results.multi_hand_landmarks and results.multi_handedness:
     temp = []
     if results.multi_hand_landmarks:
@@ -103,10 +91,11 @@ def detect_hand_position(frame, interpreter, hands, mp_hands, input_details, out
                 positions[idx] = output
             else:
                 temp.append(landmark_info)
-    
+
     if use_double and len(temp) == 2:
         input_data = np.hstack(temp).astype(np.float32)
         input_data = np.expand_dims(input_data, axis=0)
+        
         interpreter.set_tensor(input_details[0]['index'], input_data)
         interpreter.invoke()
         output_data = interpreter.get_tensor(output_details[0]['index'])
@@ -120,29 +109,29 @@ def detect_hand_position(frame, interpreter, hands, mp_hands, input_details, out
 
     return positions
 
-def test_hand_position_live(cap, hands, mp_hands, mp_drawing):
+def test_hand_position_live(cap, hands, mp_hands, mp_drawing, interpreter, input_details, output_details, use_double=True):
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             break
 
-        frame, positions = detect_hand_position_draw(frame, hands, mp_hands, mp_drawing)
-        print(positions)
+        frame, positions = detect_hand_position_draw(frame, interpreter, hands, mp_hands, mp_drawing, input_details, output_details, use_double=True)
+        # frame, positions = detect_hand_position_draw(frame, hands, mp_hands, mp_drawing, input_details, output_details)
         # Show live video in a pop-out window
         cv2.imshow("Live Hand Tracking", frame)
 
         # Press 'q' to exit
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
-def check_hand_position(octet_stream, hands, mp_hands, interpreter, input_details, output_details, use_double = False):
+##LANDMARK
+def check_hand_position(octet_stream, hands, mp_hands, interpreter, input_details, output_details, use_double = True):
     """
     Given a .bin octet stream file, determine the most common hand position
     for left and right hands in the video.
     """
     with open("temp.mp4", "wb") as f:
         f.write(octet_stream)
-    
+    print(use_double)
     cap = cv2.VideoCapture("temp.mp4")
     all_positions = []
     while cap.isOpened():
@@ -151,9 +140,10 @@ def check_hand_position(octet_stream, hands, mp_hands, interpreter, input_detail
             break 
             
         all_positions.append(detect_hand_position(frame, interpreter, hands, mp_hands, input_details, output_details, use_double = use_double))
-    
-    gesture_counts_left = [0] * (len(SINGLE_LABEL_MAP)) if not use_double else [0] * (len(DOUBLE_LABEL_MAP))
-    gesture_counts_right = [0] * (len(SINGLE_LABEL_MAP)) if not use_double else [0] * (len(DOUBLE_LABEL_MAP))
+    cv2.destroyAllWindows()
+    cap.release()
+    gesture_counts_left = [0] * (len(SINGLE_LABEL_MAP) + 1) if not use_double else [0] * (len(DOUBLE_LABEL_MAP) + 1)
+    gesture_counts_right = [0] * (len(SINGLE_LABEL_MAP) + 1) if not use_double else [0] * (len(DOUBLE_LABEL_MAP) + 1)
 
     for positions in all_positions:
         left_gesture = positions[0]
@@ -167,6 +157,7 @@ def check_hand_position(octet_stream, hands, mp_hands, interpreter, input_detail
     most_common_left = gesture_counts_left.index(max(gesture_counts_left))
 
     if not use_double:
+        print(use_double)
         most_common_right = gesture_counts_right.index(max(gesture_counts_right))
         return SINGLE_ID_TO_GESTURE[most_common_left], SINGLE_ID_TO_GESTURE[most_common_right]
 
@@ -175,20 +166,23 @@ def check_hand_position(octet_stream, hands, mp_hands, interpreter, input_detail
 def check_hand_position_api(bin_file, mp_hands, hands, interpreter, input_details, output_details, use_double):
     with open(bin_file, "rb") as f:
         octet_stream = f.read()
-    left, right = check_hand_position(octet_stream, hands, mp_hands, interpreter, input_details, output_details, use_double = use_double)
+    left, right = check_hand_position(octet_stream, hands, mp_hands, interpreter, input_details, output_details, use_double = True)
+    print(left)
     return left, right
 
-
-def main(use_double = False):
+def main(use_double = True):
+    print(DOUBLE_GESTURES, DOUBLE_ID_TO_GESTURE)
+    tflite_save_path = "./model/model_doubleTrue.tflite" 
+    interpreter, input_details, output_details = setup_model(tflite_save_path)
+    mp_hands = mp.solutions.hands
+    # Handle pause toggle message.
+    hands = mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5)
     # Open Webcam
     cap = cv2.VideoCapture(0)
     
-    tflite_save_path = "./model/model_doubleTrue.tflite" if use_double else "./model/model_doubleFalse.tflite"
-    interpreter, input_details, output_details = setup_model(tflite_save_path)
     # Test hand position
-    # test_hand_position_live(cap, hands, mp_hands, mp_drawing)
-    left, right = check_hand_position_api("test_double.bin", mp_hands, hands, interpreter, input_details, output_details, use_double = use_double)
-    print(left, right)
+    test_hand_position_live(cap, hands, mp_hands, mp_drawing, interpreter, input_details, output_details, use_double = use_double)
+    # left, right = check_hand_position_api("test_double.bin", mp_hands, hands, interpreter, input_details, output_details, use_double = use_double)
     cap.release()
     cv2.destroyAllWindows()
 
@@ -198,4 +192,5 @@ if __name__ == "__main__":
     parser.add_argument('--use_double', action='store_true', help="detect hand gestures that involve both hands")
     args = parser.parse_args()
     use_double = args.use_double
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    main(use_double = use_double)
+    # uvicorn.run(app, host="0.0.0.0", port=8000)
